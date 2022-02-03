c7570fc2ed0af73cb967079741eaacfbe9c619e4
Vladimir Petrenko <vladipetrenko@googlemail.com>
2022-02-03 23:13:46 +0100

    V 0.1.5 Updated all EditorProperties

--- addons/inventory_editor/InventoryInspectorPluginEditorItem.gd
+++ addons/inventory_editor/InventoryInspectorPluginEditorItem.gd
@@ -4,44 +4,38 @@ extends EditorProperty
 class_name InventoryInspectorEditorItem
 
 const Dropdown = preload("res://addons/inventory_editor/ui_extensions/dropdown/Dropdown.tscn")
-const ItemIcon = preload("res://addons/inventory_editor/icons/Item.png")
 
 var updating = false
-var hBox = HBoxContainer.new()
-var textureRect = TextureRect.new()
 var dropdown = Dropdown.instantiate()
 var _data: InventoryData
-var _items: Array
 var _object
 
 func set_data(data: InventoryData, object) -> void:
 	_data = data
 	_object = object
-	_update_items()
-
-func _update_items() -> void:
-	_items = []
-	for item in _data.all_items():
-		if item and item.scene:
-			var scene = load(item.scene).instantiate()
-			if _object is Item2D and scene is Node2D:
-				_items.append(item)
-			if _object is Item3D and scene is Node3D:
-				_items.append(item)
+	_data.type_added.connect(_update_dropdown)
+	_data.type_removed.connect(_update_dropdown)
+	_update_dropdown()
 	
 func _init():
-	textureRect.stretch_mode = TextureRect.STRETCH_KEEP_CENTERED
-	hBox.add_child(textureRect)
-	hBox.add_child(dropdown)
-	add_child(hBox)
+	add_child(dropdown)
 	add_focusable(dropdown)
 	dropdown.selection_changed.connect(_on_selection_changed)
 
-func _on_gui_input(event: InputEvent) -> void:
-	_update_items()
+func _update_dropdown() -> void:
 	dropdown.clear()
-	for item in _items:
-		var item_d = {"text": item.name, "value": item.uuid, "icon": item.icon }
+	for item in _data.all_items():
+		if item != null and item.scene != null:
+			var scene = load(item.scene).instantiate()
+			var item_d = DropdownItem.new(item.name, item.uuid, item.icon)
+			if _object is Item2D and scene is Node2D:
+				dropdown.add_item(item_d)
+			if _object is Item3D and scene is Node3D:
+				dropdown.add_item(item_d)
+			
+	
+	for item in _data.items:
+		var item_d = DropdownItem.new(item.name, item.uuid, item.icon)
 		dropdown.add_item(item_d)
 
 func _on_selection_changed(item: Dictionary):
@@ -49,22 +43,8 @@ func _on_selection_changed(item: Dictionary):
 		return
 	emit_changed(get_edited_property(), item.value)
 
-func update_property():
+func _update_property():
 	var new_value = get_edited_object()[get_edited_property()]
 	updating = true
-	var item = item_by_uuid(new_value)
-	var item_icon = ItemIcon
-	if item:
-		if item.icon:
-			item_icon = load(item.icon)
-		if item.text:
-			dropdown.text = item.text
-	item_icon = _data.resize_texture(item_icon, Vector2(16, 16))
-	textureRect.texture = item_icon
+	dropdown.set_selected_by_value(new_value)
 	updating = false
-
-func item_by_uuid(uuid: String) -> Dictionary:
-	for item in _items:
-		if item.uuid == uuid:
-			return {"text": item.name, "value": item.uuid, "icon": item.icon }
-	return {"text": null, "value": null, "icon": null }
--- addons/inventory_editor/InventoryInspectorPluginItem.gd
+++ addons/inventory_editor/InventoryInspectorPluginItem.gd
@@ -7,25 +7,25 @@ var _data: InventoryData
 func set_data(data: InventoryData) -> void:
 	_data = data
 
-func can_handle(object):
+func _can_handle(object):
 	return object is Item2D or object is Item3D or object is Control or object is ItemUI
 
-func parse_property(object, type, path, hint, hint_text, usage):
+func _parse_property(object: Object, type: int, name: String, hint_type: int, hint_string: String, usage_flags: int, wide: bool):
 	if type == TYPE_STRING:
-		match path:
+		match name:
 			"to_inventory", "inventory_uuid", "inventory":
 				var inspectorEditorInventory = InventoryInspectorEditorInventory.new()
 				inspectorEditorInventory.set_data(_data)
-				add_property_editor(path, inspectorEditorInventory)
+				add_property_editor(name, inspectorEditorInventory)
 				return true
 			"type":
 				var inspectorEditorType = InventoryInspectorEditorType.new()
 				inspectorEditorType.set_data(_data)
-				add_property_editor(path, inspectorEditorType)
+				add_property_editor(name, inspectorEditorType)
 				return true
 			"item_put":
 				var inspectorEditorItem = InventoryInspectorEditorItem.new()
 				inspectorEditorItem.set_data(_data, object)
-				add_property_editor(path, inspectorEditorItem)
+				add_property_editor(name, inspectorEditorItem)
 				return true
 	return false
